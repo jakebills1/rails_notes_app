2755686b2ed7940e6e49bfd4e0ad1708b67cff9c
Jake Bills <jakebills1@gmail.com>
2019-02-18 16:40:41 -0700

    all CRUD actions work

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -14,6 +14,10 @@ class NotesController < ApplicationController
     #creates a new note in memory
   end
 
+  def edit
+    @note = Note.find(params[:id])
+  end
+
   def create
     @note = Note.new(notes_params)
 
@@ -22,6 +26,20 @@ class NotesController < ApplicationController
     else
       render :new
     end
+  end
+
+  def update
+    @note = Note.find(params[:id])
+    if @note.update(notes_params)
+      redirect_to notes_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Note.find(params[:id]).destroy
+    redirect_to notes_path
   end
 
   private 
